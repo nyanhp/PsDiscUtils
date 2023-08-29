7686ee0100972c17558bee00518e7245b837360e
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2023-08-29 10:43:24 +0200

    Add new Get-PDUIsoFile command

--- PsDiscUtils/PsDiscUtils.psd1
+++ PsDiscUtils/PsDiscUtils.psd1
@@ -66,6 +66,7 @@
     # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
     CmdletsToExport      = @(
         'New-PDUIsoFile'
+        'Get-PDUIsoFile'
     )
 
     # Variables to export from this module
---
+++ lib/Iso/GetIsoFileCommand.cs
@@ -0,0 +1,25 @@
+﻿using DiscUtils.Iso9660;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Management.Automation;
+using System.Text;
+
+namespace PsDiscUtils.Iso
+{
+    [Cmdlet(VerbsCommon.Get, "PDUIsoFile")]
+    [OutputType(typeof(FileInfo))]
+    public class GetIsoFileCommand : PSCmdlet
+    {
+        [Parameter(
+            Position = 0,
+            ValueFromPipelineByPropertyName = true,
+            ValueFromPipeline = true)]
+        public string Path { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            WriteObject(new CDReader(new FileStream(Path, FileMode.Open, FileAccess.Read), true));
+        }
+    }
+}
